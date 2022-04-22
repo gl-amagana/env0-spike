9f49e5d2157078782b74430621aaafa79df600ad
gl-amagana <andrew.magana@greenlight.me>
2022-04-22 12:05:18 -0400

    another resource.

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,16 @@
 resource "aws_s3_bucket" "some_bucket" {
   bucket = "temp-env0-bucket-poc"
-
-  versioning {
-    enabled = true
-  }
 }
 
 resource "aws_s3_bucket_acl" "some_bucket_acl" {
   bucket = aws_s3_bucket.some_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "some_bucket_versioning" {
+  bucket = aws_s3_bucket.some_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
