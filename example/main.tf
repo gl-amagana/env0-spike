82b88a2161533f4d789959b29eb47c911eee43b9
gl-amagana <andrew.magana@greenlight.me>
2022-04-22 11:36:07 -0400

    test a delete?

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,12 @@
-resource "aws_s3_bucket" "some_bucket" {
-  bucket = "temp-env0-bucket-poc"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket_acl" "some_bucket_acl" {
-  bucket = aws_s3_bucket.some_bucket.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket" "some_bucket" {
+#  bucket = "temp-env0-bucket-poc"
+#
+#  versioning {
+#    enabled = true
+#  }
+#}
+#
+#resource "aws_s3_bucket_acl" "some_bucket_acl" {
+#  bucket = aws_s3_bucket.some_bucket.id
+#  acl    = "private"
+#}
