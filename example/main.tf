19135eb6b38c8d33b8c78f08a7637b96fc03e53a
gl-amagana <andrew.magana@greenlight.me>
2022-04-22 10:46:05 -0400

    wip

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,8 @@
 resource "aws_s3_bucket" "some_bucket" {
   bucket = "temp-env0-bucket-poc"
 }
+
+resource "aws_s3_bucket_acl" "some_bucket_acl" {
+  bucket = aws_s3_bucket.some_bucket.id
+  acl    = "private"
+}
