be8e1b763bb1f3d6b1bf9ee7c0bab7374aea8b87
gl-amagana <andrew.magana@greenlight.me>
2022-04-25 10:34:17 -0400

    test

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,12 @@
-#resource "aws_s3_bucket" "some_bucket" {
-#  bucket = "temp-env0-bucket-poc"
-#}
-#
-#resource "aws_s3_bucket_acl" "some_bucket_acl" {
-#  bucket = aws_s3_bucket.some_bucket.id
-#  acl    = "private"
-#}
-#
+resource "aws_s3_bucket" "some_bucket" {
+  bucket = "temp-env0-bucket-poc"
+}
+
+resource "aws_s3_bucket_acl" "some_bucket_acl" {
+  bucket = aws_s3_bucket.some_bucket.id
+  acl    = "private"
+}
+
 #resource "aws_s3_bucket_versioning" "some_bucket_versioning" {
 #  bucket = aws_s3_bucket.some_bucket.id
 #
@@ -14,7 +14,7 @@
 #    status = "Enabled"
 #  }
 #}
-#
+
 #resource "aws_iam_role" "test_role" {
 #  name = "test_role"
 #
